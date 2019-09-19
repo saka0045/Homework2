577b21213b2064661a1da051ae07790fdd0ed15a
m006703 <sakai.yuta@mayo.edu>
2019-09-18 21:58:15 -0500

    Initialized scoring matrix with gap penalties

--- needleman_wunsch.py
+++ needleman_wunsch.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import numpy as np
 
 
 def main():
@@ -18,15 +19,34 @@ def main():
     second_sequence_file_path = os.path.abspath(args.second_sequence_file)
 
     # Extract the sequence from the sequence files
-    first_sequence = extract_sequence(first_sequence_file_path)
-    second_sequence = extract_sequence(second_sequence_file_path)
+    first_sequence, first_sequence_length = extract_sequence(first_sequence_file_path)
+    second_sequence, second_sequence_length = extract_sequence(second_sequence_file_path)
 
     print("first sequence")
     print(first_sequence)
-    print(len(first_sequence))
+    print(first_sequence_length)
     print("second sequence")
     print(second_sequence)
-    print(len(second_sequence))
+    print(second_sequence_length)
+
+    # Initialize the score matrix with zeros
+    score_matrix = np.zeros([first_sequence_length + 1, second_sequence_length + 1], dtype=int)
+    print(score_matrix)
+
+    # Scoring criteria
+    match = 1
+    mismatch = -3
+    gap_penalty = -2
+
+    # Fill out the first column of each row with increasing gap penalty
+    for row in range(0, first_sequence_length + 1):
+        score_matrix[row][0] = gap_penalty * row
+
+    # Repeat for first row in each column:
+    for column in range(0, second_sequence_length + 1):
+        score_matrix[0][column] = gap_penalty * column
+
+    print(score_matrix)
 
 
 def extract_sequence(sequence_file_path):
@@ -46,7 +66,9 @@ def extract_sequence(sequence_file_path):
             continue
         else:
             sequence += line
-    return sequence
+    sequence_length = len(sequence)
+    sequence_file.close()
+    return sequence, sequence_length
 
 
 if __name__ == "__main__":
