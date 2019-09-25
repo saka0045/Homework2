1a15f08952ec28a5eff8ff2ae39b29c9c72bab58
Yuta Sakai <sakai.yuta@mayo.edu>
2019-09-25 07:15:12 -0500

    Added result files after running run_permutate

---
+++ run_permutate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/biotools/biotools/python/3.6.3/bin/python3.6 /home/m006703/CSCI5481/Homework2/run_permutate.py -a /home/m006703/CSCI5481/Homework2/Human_PAX.fa -b /home/m006703/CSCI5481/Homework2/Fly_PAX.fa -o PAX_permutate.txt
