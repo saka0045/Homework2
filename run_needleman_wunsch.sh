2a0a6eaffa035bd64e29cb4d7c3c4e7bc41bdcd2
Yuta Sakai <sakai.yuta@mayo.edu>
2019-09-27 16:09:58 -0500

    Added bash script to run needleman_wunsch.py

---
+++ run_needleman_wunsch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/biotools/biotools/python/3.6.3/bin/python3.6 /home/m006703/CSCI5481/Homework2/needleman_wunsch.py -a /home/m006703/CSCI5481/Homework2/Human_HOX.fa -b /home/m006703/CSCI5481/Homework2/Fly_HOX.fa -t /home/m006703/CSCI5481/Homework2/Match_HOX.txt > /home/m006703/test.txt
