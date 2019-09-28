92ae5c467aa85fe2590a61139abfff27c433ca17
Yuta Sakai <sakai.yuta@mayo.edu>
2019-09-27 19:53:55 -0500

    added aligned TITIN sequences

--- run_needleman_wunsch.sh
+++ run_needleman_wunsch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/biotools/biotools/python/3.6.3/bin/python3.6 /home/m006703/CSCI5481/Homework2/needleman_wunsch.py -a /home/m006703/CSCI5481/Homework2/Human_HOX.fa -b /home/m006703/CSCI5481/Homework2/Fly_HOX.fa -t /home/m006703/CSCI5481/Homework2/Match_HOX.txt > /home/m006703/test.txt
+/biotools/biotools/python/3.6.3/bin/python3.6 /home/m006703/CSCI5481/Homework2/needleman_wunsch.py -a /home/m006703/CSCI5481/Homework2/TITIN_Human.fa -b /home/m006703/CSCI5481/Homework2/TITIN_Mouse.fa -t /home/m006703/CSCI5481/Homework2/TITIN_Match.txt > /home/m006703/CSCI5481/Homework2/Aligned_TITIN.txt
